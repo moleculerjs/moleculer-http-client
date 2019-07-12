5a560b52ecc171cbed4df81f15dd57933a97ffc3
AndreMaz <andre.maz.90@gmail.com>
2019-07-12 17:40:23 +0100

    Update readme

--- README.md
+++ README.md
@@ -12,6 +12,50 @@ npm install moleculer-http-client --save
 ```
 ## Usage
 
+**Example**
+```js
+const { ServiceBroker } = require("moleculer");
+const HTTPClientService = require("../../index");
+
+// Create broker
+let broker = new ServiceBroker({
+  namespace: 'client',
+  nodeID: "namespace"
+});
+
+broker.createService({
+  name: "http",
+
+  // Load HTTP Client Service  
+  mixins: [HTTPClientService],
+  
+  settings: {
+    // Only load HTTP action
+    got: { includeMethods: ["get"] }
+  }
+});
+
+// Start server
+broker.start().then(() => {
+  broker
+    .call("http.get", {
+      url: "https://httpbin.org/json",
+      opt: { json: true }
+    })
+    .then(res => broker.logger.info(res.body))
+    .catch(error => broker.logger.error(error));
+});
+```
+
+**Result**
+```bash
+INFO  node/HTTP: => HTTP GET to https://httpbin.org/json
+
+INFO  node/HTTP: <= HTTP GET to "https://httpbin.org/json" returned with status code 200
+
+INFO  node/BROKER: { slideshow: { author: 'Yours Truly', date: 'date of publication', slides: [ [Object], [Object] ], title: 'Sample Slide Show' } }
+
+```
 
 ## Test
 ```
--- examples/actions/index.js
+++ examples/actions/index.js
@@ -1,10 +1,7 @@
 "use strict";
 
 const { ServiceBroker } = require("moleculer");
-const MyService = require("../../index");
-const { MoleculerGotError } = require("../../src/errors");
-
-const fs = require("fs");
+const HTTPClientService = require("../../index");
 
 // Create broker
 let broker = new ServiceBroker({
@@ -12,59 +9,23 @@ let broker = new ServiceBroker({
 });
 
 // Load my service
-let s = broker.createService({
-  name: "gotMixed",
+broker.createService({
+  name: "http",
 
-  mixins: [MyService],
+  mixins: [HTTPClientService],
 
   settings: {
-    got: {
-      logging: false,
-      includeMethods: ["get", "post"],
-      defaultOptions: {}
-    }
-  },
-
-  actions: {
-    async get() {
-      try {
-        console.log(this.settings.got.defaultOptions);
-
-        /*
-        let res = await this._get("http://httpbin.org/status/200", {
-          json: true
-        });
-        */
-
-        return this.settings.got.defaultOptions;
-      } catch (error) {
-        // console.log(error);
-        throw "Got an ERROR";
-      }
-    },
-
-    getStream() {
-      return new Promise((resolve, reject) => {
-        let streamRequest = this._get("https://httpbin.org/html", {
-          stream: true
-        });
-
-        streamRequest.pipe(fs.createWriteStream("index.html"));
-
-        streamRequest.on("end", () => {
-          resolve("done");
-        });
-      });
-    }
+    got: { includeMethods: ["get"] }
   }
 });
 
 // Start server
 broker.start().then(() => {
-  // Call action
-  // broker.repl();
   broker
-    .call("gotMixed.get", { name: "John MIXIN Doe" })
-    .then()
-    .catch(broker.logger.error);
+    .call("http.get", {
+      url: "https://httpbin.org/json",
+      opt: { json: true }
+    })
+    .then(res => broker.logger.info(res.body))
+    .catch(error => broker.logger.error(error));
 });
